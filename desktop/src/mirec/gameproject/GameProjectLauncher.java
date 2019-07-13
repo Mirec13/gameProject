4b5b9edd94cd0ca9a79d5be2ccd1f83fddaa7d66
Mirec13 <balgamiroslav@gmail.com>
2019-07-13 13:19:40 +0200

    my own launcher GameProjectLauncher

---
+++ desktop/src/mirec/gameproject/GameProjectLauncher.java
@@ -0,0 +1,12 @@
+package mirec.gameproject;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class GameProjectLauncher {
+    public static void main (String[] args) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyGdxGame(), config);
+    }
+}

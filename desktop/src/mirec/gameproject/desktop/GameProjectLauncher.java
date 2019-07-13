b4d1c3465186a2400644d83a4fe27aca64a9a858
Mirec13 <balgamiroslav@gmail.com>
2019-07-13 13:22:56 +0200

    change name of the package

--- desktop/src/mirec/gameproject/desktop/GameProjectLauncher.java
+++ desktop/src/mirec/gameproject/desktop/GameProjectLauncher.java
@@ -1,4 +1,4 @@
-package mirec.gameproject;
+package mirec.gameproject.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;

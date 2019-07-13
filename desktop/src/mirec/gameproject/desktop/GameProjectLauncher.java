7c2c40e6ac50e2a8a30d94b9b693d2686d6cad69
Mirec13 <balgamiroslav@gmail.com>
2019-07-13 13:33:57 +0200

    created my template for game

--- desktop/src/mirec/gameproject/desktop/GameProjectLauncher.java
+++ desktop/src/mirec/gameproject/desktop/GameProjectLauncher.java
@@ -3,6 +3,7 @@ package mirec.gameproject.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
+import mirec.gameproject.game.GameProjectGame;
 
 public class GameProjectLauncher {
     public static void main (String[] args) {

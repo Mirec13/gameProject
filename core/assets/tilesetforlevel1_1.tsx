e52fd4e6016c5631a931aaff01e7a8d187919374
Mirec13 <balgamiroslav@gmail.com>
2019-07-27 15:17:23 +0200

    new tile map

---
+++ core/assets/tilesetforlevel1_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="field4" tilewidth="32" tileheight="32" tilecount="2220" columns="50">
+ <image source="field4.jpeg" width="1600" height="480"/>
+</tileset>

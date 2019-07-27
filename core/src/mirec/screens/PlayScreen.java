19e415767c80f97f15c8a5d065397a599f8baef2
Mirec13 <balgamiroslav@gmail.com>
2019-07-27 15:19:18 +0200

    loaded tilemap

--- core/src/mirec/screens/PlayScreen.java
+++ core/src/mirec/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import mirec.gameproject.GameProjectGame;
@@ -15,27 +18,65 @@ public class PlayScreen implements Screen {
     private GameProjectGame game;
     private OrthographicCamera gameCam;
     private Viewport gameViewPort;
-    private Hud hud;
+    private Hud hud;//heads up display
+    private TmxMapLoader loadLevel;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
 
+
+    //new game
     public PlayScreen (GameProjectGame game) {
         this.game = game;
         gameCam = new OrthographicCamera();
-        gameViewPort = new FitViewport(GameProjectGame.width, GameProjectGame.height, gameCam);
+        gameViewPort = new FitViewport(GameProjectGame.WIDTH, GameProjectGame.HEIGHT, gameCam);
         hud = new Hud(game.batch);
+
+        loadLevel = new TmxMapLoader();
+        map = loadLevel.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gameViewPort.getWorldWidth() / 2, gameViewPort.getWorldHeight() / 2, 0);
+    }
+
+    //load new level or resume where you started
+    public PlayScreen (GameProjectGame game, String level) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gameViewPort = new FitViewport(GameProjectGame.WIDTH, GameProjectGame.HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+
+        //TO DO
     }
 
     @Override
     public void show() {
+    }
 
+
+    public void handleInput(float delta) {
+        if (Gdx.input.isTouched()) {
+            gameCam.position.x += 200 * delta;
+            System.out.println(delta);
+        }
+    }
+
+    public void update(float delta) {
+        handleInput(delta);
+
+        gameCam.update();
+        renderer.setView(gameCam);
     }
 
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+
+        renderer.render();
 
         game.batch.begin();
         game.batch.end();
